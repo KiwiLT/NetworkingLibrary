cd610c3bb3ab44d84f9e8cfcd1bce54657bfccca
KiwiLT <1018958@hr.nl>
2021-10-18 12:32:59 +0200

    Progress in bookhelper

--- LibBookHelper/BookHelper.cs
+++ LibBookHelper/BookHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Text.Json;
 using System.Net;
 using System.Net.Sockets;
@@ -30,8 +31,11 @@ namespace BookHelper
         public IPAddress localIP;
         public IPAddress serverIP;
         public Setting settings;
+        public string bookinfo;
         public int Queue;
         public string configFile = @"../ClientServerConfig.json";
+        public string bookFile = @"./Books.json";
+        public List<BookData> books;
 
         public SequentialHelper()
         {
@@ -46,6 +50,10 @@ namespace BookHelper
                 this.listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 listeningSocket.Bind(localEndPoint);
                 this.Queue = settings.ServerListeningQueue;
+
+                string bookContent = File.ReadAllText(bookFile);
+                this.books = JsonSerializer.Deserialize<List<BookData>>(bookContent);
+
             } catch (Exception e){
                 Console.Out.WriteLine("[Book Helper server Exception] {0}", e.Message);
             }
@@ -53,7 +61,93 @@ namespace BookHelper
 
         public void start()
         {
-            //todo: implement the body. Add extra fields and methods to the class if needed
+            Console.WriteLine("Connecting to server...");
+            listeningSocket.Listen(Queue);
+            bookSocket = listeningSocket.Accept();
+            Console.WriteLine("Connected!");
+
+            while (true)
+            {
+                var msg = new byte[1000];
+                var buffer = new byte[1000];
+                Console.WriteLine("Waiting for messages from the client...");
+                bookSocket.Receive(buffer);
+                var received = BytesToMessage(buffer);
+                if(received.Type == MessageType.EndCommunication){
+                    Console.WriteLine("Closing the socket...");
+                    bookSocket.Close();
+                    break;
+                } else if(received.Type == MessageType.BookInquiry){
+                    Console.WriteLine("Book Inquiry received!");
+
+                }
+            }
+        }
+
+//helper functions
+        public byte[] messageToBytes(Message msg)
+        {
+            return Encoding.ASCII.GetBytes(messageToString(msg));
+        }
+
+        public string messageToString(Message msg)
+        {
+            switch (msg.Type)
+            {
+                case (MessageType.Hello):
+                    return "Hello" + "," + msg.Content;
+                case (MessageType.Welcome):
+                    return "Welcome" + "," + msg.Content;
+                case (MessageType.BookInquiry):
+                    return "BookInquiry" + "," + msg.Content;
+                case (MessageType.UserInquiry):
+                    return "UserInquiry" + "," + msg.Content;
+                case (MessageType.BookInquiryReply):
+                    return "BookInquiryReply" + "," + msg.Content;
+                case (MessageType.UserInquiryReply):
+                    return "UserInquiryReply" + "," + msg.Content;
+                case (MessageType.EndCommunication):
+                    return "EndCommunication" + "," + msg.Content;
+                case (MessageType.Error):
+                    return "Error" + "," + msg.Content;
+                case (MessageType.NotFound):
+                    return "NotFound" + "," + msg.Content;
+                default:
+                    return "";
+            }
+
+
+        }
+
+        public Message BytesToMessage(byte[] bytes)
+        {
+            var msg = new Message();
+            string fullstring = Encoding.ASCII.GetString(bytes);
+            string[] subs = fullstring.Split(",");
+            string type = subs[0];
+            string content = subs[1];
+            switch (type)
+            {
+                case ("Hello"):
+                    msg.Type = MessageType.Hello; break;
+                case("Welcome"):
+                    msg.Type = MessageType.Welcome; break;
+                case("BookInquiry"):
+                    msg.Type = MessageType.BookInquiry; break;
+                case("UserInquiry"):
+                    msg.Type = MessageType.UserInquiry; break;
+                case("BookInquiryReply"):
+                    msg.Type = MessageType.BookInquiryReply; break;
+                case("UserInquiryReply"):
+                    msg.Type = MessageType.UserInquiryReply; break;
+                case("EndCommunication"):
+                    msg.Type = MessageType.EndCommunication; break;
+                case("Error"):
+                    msg.Type = MessageType.Error; break;
+                case("NotFound"):
+                    msg.Type = MessageType.NotFound; break;
+            }
+            return msg;
         }
     }
 }
