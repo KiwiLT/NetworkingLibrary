79c0548bb4c039c25ec87a2f7a9f85d4e0a24358
KiwiLT <1018958@hr.nl>
2021-10-18 11:02:11 +0200

    second commit

---
+++ LibClient/Client.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using LibData;
+
+
+namespace LibClient
+{
+    // Note: Do not change this class 
+    public class Setting
+    {
+        public int ServerPortNumber { get; set; }
+        public int BookHelperPortNumber { get; set; }
+        public int UserHelperPortNumber { get; set; }
+        public string ServerIPAddress { get; set; }
+        public string BookHelperIPAddress { get; set; }
+        public string UserHelperIPAddress { get; set; }
+        public int ServerListeningQueue { get; set; }
+    }
+
+    // Note: Do not change this class 
+    public class Output
+    {
+        public string Client_id { get; set; } // the id of the client that requests the book
+        public string BookName { get; set; } // the name of the book to be reqyested
+        public string Status { get; set; } // final status received from the server
+        public string BorrowerName { get; set; } // the name of the borrower in case the status is borrowed, otherwise null
+        public string BorrowerEmail { get; set; } // the email of the borrower in case the status is borrowed, otherwise null
+    }
+
+    // Note: Complete the implementation of this class. You can adjust the structure of this class.
+    public class SimpleClient
+    {
+        // some of the fields are defined. 
+        public Output result;
+        public Socket clientSocket;
+        public IPEndPoint serverEndPoint;
+        public IPAddress ipAddress;
+        public Setting settings;
+        public string client_id;
+        private string bookName;
+        // all the required settings are provided in this file
+        public string configFile = @"../ClientServerConfig.json";
+        //public string configFile = @"../../../../ClientServerConfig.json"; // for debugging
+
+        // todo: add extra fields here in case needed 
+
+        /// <summary>
+        /// Initializes the client based on the given parameters and seeting file.
+        /// </summary>
+        /// <param name="id">id of the clients provided by the simulator</param>
+        /// <param name="bookName">name of the book to be requested from the server, provided by the simulator</param>
+        public SimpleClient(int id, string bookName)
+        {
+
+            this.bookName = bookName;
+            this.client_id = "Client " + id.ToString();
+            this.result = new Output();
+            result.BookName = bookName;
+            result.Client_id = this.client_id;
+            // read JSON directly from a file
+            try
+            {
+                string configContent = File.ReadAllText(configFile);
+                this.settings = JsonSerializer.Deserialize<Setting>(configContent);
+                this.ipAddress = IPAddress.Parse(settings.ServerIPAddress);
+                this.serverEndPoint = new IPEndPoint(ipAddress, settings.ServerPortNumber);
+                this.clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("[Client Exception] {0}", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Establishes the connection with the server and requests the book according to the specified protocol.
+        /// Note: The signature of this method must not change.
+        /// </summary>
+        /// <returns>The result of the request</returns>
+        public Output start()
+        {
+            //initialize variables, create connection
+            byte[] buffer = new byte[1000];
+            byte[] msg = new byte[1000];
+
+            //if this is the first client, create a connection
+            if (client_id == "Client 0")
+            {
+                clientSocket.Connect(serverEndPoint);
+            }
+
+            //if this is client -1, send endcommunication message and close the socket.
+            if (client_id == "Client -1"){
+                var endcomm = new Message();
+                endcomm.Type = MessageType.EndCommunication;
+                endcomm.Content = "";
+                msg = messageToBytes(endcomm);
+                clientSocket.Send(msg);
+                clientSocket.Close();
+            }
+
+            //Client starts with hello message
+            var hello = new Message();
+            hello.Type = MessageType.Hello;
+            hello.Content = client_id;
+            msg =   messageToBytes(hello);
+            clientSocket.Send(msg);
+
+            //Then the client waits until he receives 'welcome' message
+            int b = clientSocket.Receive(buffer);
+            var welcome = BytesToMessage(buffer);
+            //if the received message is error this function will return an error.
+            if (welcome.Type == MessageType.Error)
+            {
+                result.Status = welcome.Content;
+                result.BorrowerEmail = null;
+                result.BorrowerName = null;
+                return result;
+            }
+
+            //the client will send the BookInquiry message, asking for a book by sending bookname
+            var bookinquiry = new Message();
+            bookinquiry.Type = MessageType.BookInquiry;
+            bookinquiry.Content = this.bookName;
+            msg = messageToBytes(bookinquiry);
+            clientSocket.Send(msg);
+
+            //the client will wait unitl he receives the status of the book
+            buffer = null;
+            b = clientSocket.Receive(buffer);
+            var bookinquiryreply = BytesToMessage(buffer);
+            if (bookinquiryreply.Type == MessageType.NotFound)
+            {
+                result.Status = "BookNotFound";
+                result.BorrowerEmail = null;
+                result.BorrowerName = null;
+                return result;
+            } else if (bookinquiryreply.Type == MessageType.Error)
+            {
+                result.Status = bookinquiryreply.Content;
+                result.BorrowerEmail = null;
+                result.BorrowerName = null;
+                return result;
+            }
+            //change content of the message from jsonstring to an BookData object
+            string jsonstring = bookinquiryreply.Content;
+            BookData myBook = JsonSerializer.Deserialize<BookData>(jsonstring);
+
+            //if the book is available, status will be "Available", borrower information will be null
+            if (myBook.Status == "Available")
+            {
+                result.Status = "Available";
+                result.BorrowerEmail = null;
+                result.BorrowerName = null;
+                return result;
+            //if the book is borrowed, the client will request the user information, using the user_id in the status of myBook
+            } else 
+            {
+                var userinquiry = new Message();
+                userinquiry.Type = MessageType.UserInquiry;
+                userinquiry.Content = myBook.BorrowedBy;
+                msg = messageToBytes(userinquiry);
+                clientSocket.Send(msg);
+                buffer = null;
+                b = clientSocket.Receive(buffer);
+                var userinquiryreply = BytesToMessage(buffer);
+                if (userinquiryreply.Type == MessageType.Error){
+                    result.Status = userinquiryreply.Content;
+                    result.BorrowerEmail = null;
+                    result.BorrowerName = null;
+                    return result;
+                }
+                jsonstring = userinquiryreply.Content;
+                UserData myUser = JsonSerializer.Deserialize<UserData>(jsonstring);
+
+                //build output
+                result.BorrowerName = myUser.Name;
+                result.BorrowerEmail = myUser.Email;
+                result.Status = "Borrowed";
+                return result;
+            }
+        }
+
+
+//helper functions
+        public byte[] messageToBytes(Message msg)
+        {
+            return Encoding.ASCII.GetBytes(messageToString(msg));
+        }
+
+        public string messageToString(Message msg)
+        {
+            switch (msg.Type)
+            {
+                case (MessageType.Hello):
+                    return "Hello" + "," + msg.Content;
+                case (MessageType.Welcome):
+                    return "Welcome" + "," + msg.Content;
+                case (MessageType.BookInquiry):
+                    return "BookInquiry" + "," + msg.Content;
+                case (MessageType.UserInquiry):
+                    return "UserInquiry" + "," + msg.Content;
+                case (MessageType.BookInquiryReply):
+                    return "BookInquiryReply" + "," + msg.Content;
+                case (MessageType.UserInquiryReply):
+                    return "UserInquiryReply" + "," + msg.Content;
+                case (MessageType.EndCommunication):
+                    return "EndCommunication" + "," + msg.Content;
+                case (MessageType.Error):
+                    return "Error" + "," + msg.Content;
+                case (MessageType.NotFound):
+                    return "NotFound" + "," + msg.Content;
+                default:
+                    return "";
+            }
+
+
+        }
+
+        public Message BytesToMessage(byte[] bytes)
+        {
+            var msg = new Message();
+            string fullstring = Encoding.ASCII.GetString(bytes);
+            string[] subs = fullstring.Split(",");
+            string type = subs[0];
+            string content = subs[1];
+            switch (type)
+            {
+                case ("Hello"):
+                    msg.Type = MessageType.Hello; break;
+                case("Welcome"):
+                    msg.Type = MessageType.Welcome; break;
+                case("BookInquiry"):
+                    msg.Type = MessageType.BookInquiry; break;
+                case("UserInquiry"):
+                    msg.Type = MessageType.UserInquiry; break;
+                case("BookInquiryReply"):
+                    msg.Type = MessageType.BookInquiryReply; break;
+                case("UserInquiryReply"):
+                    msg.Type = MessageType.UserInquiryReply; break;
+                case("EndCommunication"):
+                    msg.Type = MessageType.EndCommunication; break;
+                case("Error"):
+                    msg.Type = MessageType.Error; break;
+                case("NotFound"):
+                    msg.Type = MessageType.NotFound; break;
+            }
+            return msg;
+        }
+
+    }
+}
---
+++ LibServer/LibServer.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Text.Json;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using LibData;
+
+
+namespace LibServer
+{
+    // Note: Do not change this class.
+    public class Setting
+    {
+        public int ServerPortNumber { get; set; }
+        public int BookHelperPortNumber { get; set; }
+        public int UserHelperPortNumber { get; set; }
+        public string ServerIPAddress { get; set; }
+        public string BookHelperIPAddress { get; set; }
+        public string UserHelperIPAddress { get; set; }
+        public int ServerListeningQueue { get; set; }
+    }
+
+
+    // Note: Complete the implementation of this class. You can adjust the structure of this class. 
+    public class SequentialServer
+    {
+        public Socket bookSocket;
+        public Socket userSocket;
+        public Socket listeningSocket;
+        public Socket serverSocket;
+        public IPEndPoint bookEndPoint;
+        public IPEndPoint userEndPoint;
+        public IPEndPoint localEndPoint;
+        public IPAddress bookIP;
+        public IPAddress userIP;
+        public IPAddress localIP;
+        public Setting settings;
+        public int Queue;
+        public string configFile = @"../ClientServerConfig.json";
+
+        public SequentialServer()
+        {
+            try
+            {
+                string configContent = File.ReadAllText(configFile);
+                this.settings = JsonSerializer.Deserialize<Setting>(configContent);
+                this.bookIP = IPAddress.Parse(settings.BookHelperIPAddress);
+                this.userIP = IPAddress.Parse(settings.UserHelperIPAddress);
+                this.localIP = IPAddress.Parse(settings.ServerIPAddress);
+                this.bookEndPoint = new IPEndPoint(bookIP, settings.BookHelperPortNumber);
+                this.userEndPoint = new IPEndPoint(userIP, settings.UserHelperPortNumber);
+                this.localEndPoint = new IPEndPoint(localIP, settings.ServerPortNumber);
+                this.bookSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                this.userSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                this.listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                listeningSocket.Bind(localEndPoint);
+                this.Queue = settings.ServerListeningQueue;
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("[Server Exception] {0}", e.Message);
+            }
+        }
+
+        public void start()
+        {   
+            byte[] buffer = new byte[1000];
+            byte[] msg = new byte[1000];
+            int b;
+
+            Console.WriteLine("Connecting to Book helper server...");
+            bookSocket.Connect(bookEndPoint);
+            Console.WriteLine("Connected!");
+            Console.WriteLine("Connecting to User helper server...");
+            userSocket.Connect(userEndPoint);
+            Console.WriteLine("Connected!");
+
+            listeningSocket.Listen(Queue);
+            Console.WriteLine("\nConnecting to client...");
+            serverSocket = listeningSocket.Accept();
+            Console.WriteLine("Connected!");
+
+            while (true)
+            {
+                buffer = null;
+                msg = null;
+                b = serverSocket.Receive(buffer);
+                var hello = BytesToMessage(buffer);
+
+                if (hello.Type == MessageType.EndCommunication)
+                {
+                    var endcomm = new Message();
+                    endcomm.Type = MessageType.EndCommunication;
+                    endcomm.Content = "";
+                    msg = messageToBytes(endcomm);
+                    serverSocket.Close();
+                    listeningSocket.Close();
+                    bookSocket.Send(msg);
+                    bookSocket.Close();
+                    userSocket.Send(msg);
+                    userSocket.Close();
+                }
+                if (hello.Type != MessageType.Hello)
+                {
+                    var error = new Message();
+                    error.Type = MessageType.Error;
+                    error.Content = "Error: didnt receive hello message";
+                    msg = messageToBytes(error);
+                    serverSocket.Send(msg);
+                    continue;
+                }
+                
+                //send back welcome
+                var welcome = new Message();
+                welcome.Type = MessageType.Welcome;
+                welcome.Content = "";
+                msg = messageToBytes(welcome);
+                serverSocket.Send(msg);
+
+                //get book inquiry and send it to the book helper server
+                b = serverSocket.Receive(buffer);
+                bookSocket.Send(buffer);
+                //Send the message received from the book server to the client
+                b = bookSocket.Receive(buffer);
+                serverSocket.Send(buffer);
+                Message bookinquiryreply = BytesToMessage(buffer);
+
+                //stop and go to the next loop if the book is not found or the book is available. otherwise wait for the userinquiry
+                if (bookinquiryreply.Type == MessageType.NotFound)
+                {
+                    continue;
+                }
+                BookData myBook = JsonSerializer.Deserialize<BookData>(bookinquiryreply.Content);
+                if (myBook.Status == "Available")
+                {
+                    continue;
+                }
+
+                b = serverSocket.Receive(buffer);
+                userSocket.Send(buffer);
+                b = userSocket.Receive(buffer);
+                serverSocket.Send(buffer);
+            }
+
+        }
+
+
+
+//helper functions
+        public byte[] messageToBytes(Message msg)
+        {
+            return Encoding.ASCII.GetBytes(messageToString(msg));
+        }
+
+        public string messageToString(Message msg)
+        {
+            switch (msg.Type)
+            {
+                case (MessageType.Hello):
+                    return "Hello" + "," + msg.Content;
+                case (MessageType.Welcome):
+                    return "Welcome" + "," + msg.Content;
+                case (MessageType.BookInquiry):
+                    return "BookInquiry" + "," + msg.Content;
+                case (MessageType.UserInquiry):
+                    return "UserInquiry" + "," + msg.Content;
+                case (MessageType.BookInquiryReply):
+                    return "BookInquiryReply" + "," + msg.Content;
+                case (MessageType.UserInquiryReply):
+                    return "UserInquiryReply" + "," + msg.Content;
+                case (MessageType.EndCommunication):
+                    return "EndCommunication" + "," + msg.Content;
+                case (MessageType.Error):
+                    return "Error" + "," + msg.Content;
+                case (MessageType.NotFound):
+                    return "NotFound" + "," + msg.Content;
+                default:
+                    return "";
+            }
+
+
+        }
+
+        public Message BytesToMessage(byte[] bytes)
+        {
+            var msg = new Message();
+            string fullstring = Encoding.ASCII.GetString(bytes);
+            string[] subs = fullstring.Split(",");
+            string type = subs[0];
+            string content = subs[1];
+            switch (type)
+            {
+                case ("Hello"):
+                    msg.Type = MessageType.Hello; break;
+                case("Welcome"):
+                    msg.Type = MessageType.Welcome; break;
+                case("BookInquiry"):
+                    msg.Type = MessageType.BookInquiry; break;
+                case("UserInquiry"):
+                    msg.Type = MessageType.UserInquiry; break;
+                case("BookInquiryReply"):
+                    msg.Type = MessageType.BookInquiryReply; break;
+                case("UserInquiryReply"):
+                    msg.Type = MessageType.UserInquiryReply; break;
+                case("EndCommunication"):
+                    msg.Type = MessageType.EndCommunication; break;
+                case("Error"):
+                    msg.Type = MessageType.Error; break;
+                case("NotFound"):
+                    msg.Type = MessageType.NotFound; break;
+            }
+            return msg;
+        }
+    }
+
+}
+
+
+
