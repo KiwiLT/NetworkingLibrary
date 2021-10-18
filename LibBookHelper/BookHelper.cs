2f798c64e8f2e442babd915d44758c3280224f5f
KiwiLT <1018958@hr.nl>
2021-10-18 12:17:30 +0200

    initialization and variables of BookHelper

--- LibBookHelper/BookHelper.cs
+++ LibBookHelper/BookHelper.cs
@@ -23,9 +23,32 @@ namespace BookHelper
     // Note: Complete the implementation of this class. You can adjust the structure of this class.
     public class SequentialHelper
     {
+        public Socket bookSocket;
+        public Socket listeningSocket;
+        public IPEndPoint localEndPoint;
+        public IPEndPoint serverEndPoint;
+        public IPAddress localIP;
+        public IPAddress serverIP;
+        public Setting settings;
+        public int Queue;
+        public string configFile = @"../ClientServerConfig.json";
+
         public SequentialHelper()
         {
-            //todo: implement the body. Add extra fields and methods to the class if needed
+            try
+            {
+                string configContent = File.ReadAllText(configFile);
+                this.settings = JsonSerializer.Deserialize<Setting>(configContent);
+                this.serverIP = IPAddress.Parse(settings.ServerIPAddress);
+                this.localIP = IPAddress.Parse(settings.BookHelperIPAddress);
+                this.localEndPoint = new IPEndPoint(localIP, settings.BookHelperPortNumber);
+                this.serverEndPoint = new IPEndPoint(serverIP, settings.ServerPortNumber);
+                this.listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                listeningSocket.Bind(localEndPoint);
+                this.Queue = settings.ServerListeningQueue;
+            } catch (Exception e){
+                Console.Out.WriteLine("[Book Helper server Exception] {0}", e.Message);
+            }
         }
 
         public void start()
